5c38c083420a37f23ad8275571a89d50a738fc8c
Estefany Nieto <119767197+EstefanyNieto@users.noreply.github.com>
2023-10-24 21:28:56 -0400

    new change was made

---
+++ js/task-01.js
@@ -0,0 +1,23 @@
+const list = document.querySelector('#categories');
+let categoriesTotal = list.children.length;
+console.log(`Number of categories: ${categoriesTotal}`);
+
+const listCategories = document.querySelectorAll('.item');
+for(const category of listCategories){
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  const categoryItems = category.querySelectorAll("li")
+  console.log(`Element: ${categoryItems.length}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
