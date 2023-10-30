0e86491077c62e7548b8e03808e78c7227ddf5bb
Estefany Nieto <119767197+EstefanyNieto@users.noreply.github.com>
2023-10-29 21:02:37 -0400

    correction on js1

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,13 @@
 const list = document.querySelector('#categories');
 let categoriesTotal = list.children.length;
-console.log(`Number of categories: ${categoriesTotal}`);
+console.log("Number of categories:", categoriesTotal);
 
 const listCategories = document.querySelectorAll('.item');
 for(const category of listCategories){
   console.log(`Category: ${category.firstElementChild.textContent}`);
   const categoryItems = category.querySelectorAll("li")
-  console.log(`Element: ${categoryItems.length}`)
+  console.log("Element:", categoryItems.length);
 }
-
-
 
 
 
